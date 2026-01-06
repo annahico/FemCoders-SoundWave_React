3742d3ab2a6a9b5e21f28bde4e01824f4d7a4ea3
annahico <annahico@gmail.com>
2026-01-06 12:48:29 +0100

    feat: installed all dependences and created basic structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>🎵 FemCoders SoundWave 🎶</h1>
+      <p>Tu aplicación de música con React 19 está lista</p>
+    </div>
+  )
+}
+
+export default App
