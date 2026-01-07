f19ef6e443416338fb19705cf896354f2eb7b542
annahico <annahico@gmail.com>
2026-01-07 15:33:50 +0100

    feat: added home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '../components/atoms/Button';
+import girlImage from '../img/landing-page-girl.png';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
-            <h1>Home Page (Under Construction)</h1>
-            <p>Here I can see how my button-engine Atom looks:</p>
+        <section className="home-container">
+            {/* Sticker Sheet */}
+            <div className="circle circle-pink"></div>
+            <div className="circle circle-blue-top"></div>
+            <div className="circle circle-blue-bottom"></div>
 
-            {/* Uso el componente Button */}
-            <Button onClick={() => alert('¡Botón funcionando!')}>
-                Join Now
-            </Button>
-        </div>
+            <div className="home-content">
+                <div className="home-image">
+                    <img src={girlImage} alt="Girl with headphones" />
+                </div>
+
+                <div className="home-text">
+                    <h1>Feel The Music</h1>
+                    <p>Stream over 20 thousand songs with one click</p>
+                    <Link to="/join">
+                        <Button>Join Now</Button>
+                    </Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
